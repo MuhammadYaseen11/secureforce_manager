a39a4c22f2dca660370872314871f4255c5ac3b7
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2025-04-30 14:21:09 +0100

    update mian, home_page.dart

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Welcome, [Employer Name]',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('View Staff')),
+              ElevatedButton(onPressed: () {}, child: Text('Assign Shift')),
+              ElevatedButton(onPressed: () {}, child: Text('Generate Report')),
+            ],
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Staff Overview',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Upcoming Shifts',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Notifications',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
